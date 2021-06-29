527369af3e74b7f8300e1390a8fe2a2ed1ea27a4
Elizabeth Doherty <elizabethcaitlinjen@gmail.com>
2021-06-29 15:48:40 -0400

    Initial Commit

---
+++ Technical4.5/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Technical4.5
+//
+//  Created by Elizabeth Doherty on 5/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var `switch`: UISwitch!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func button(_ sender: UIButton) {
+        label.text = "You changed me"
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+//this is a change
+    //and another change
+
+}
+
